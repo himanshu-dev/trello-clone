b45c891ef2f2e555ae202dc2cbf1a1d43bcf95f3
himanshu <h47290@gmail.com>
2024-05-19 16:21:19 +0530

    refactor: code cleanup

--- src/utils/dnd.ts
+++ src/utils/dnd.ts
@@ -34,28 +34,26 @@ export const useItemDrag = (item: DragItem) => {
 }
 
 export const useColumnDrop = (item: DragItem) => {
-  const { dispatch, draggedItem, lists } = useAppState()
+  const { dispatch, draggedItem } = useAppState()
   const [, drop] = useDrop({
     accept: ['COLUMN', 'CARD'],
     hover: throttle(200, () => {
       if (isDraggedItem(draggedItem, item.type, item.id)) return
+
       if (draggedItem?.type === 'COLUMN') {
         dispatch(moveList(draggedItem.id, item.id))
-      } else {
-        const _draggedItem = draggedItem as CardDragItem
-        const { id, columnId } = item as CardDragItem
-
-        if (_draggedItem.columnId === id) {
-          return
-        }
-
-        const tasks = lists.find(list => list.id === columnId)?.tasks
-        if (tasks && tasks.length) {
-          return
-        }
-        dispatch(moveTask(_draggedItem.id, null, _draggedItem.columnId, id))
-        dispatch(setDraggedItem({ ..._draggedItem, columnId: id }))
+        return
       }
+
+      const _draggedItem = draggedItem as CardDragItem
+      const { id } = item as CardDragItem
+
+      if (_draggedItem.columnId === id) {
+        return
+      }
+
+      dispatch(moveTask(_draggedItem.id, null, _draggedItem.columnId, id))
+      dispatch(setDraggedItem({ ..._draggedItem, columnId: id }))
     }),
   })
   return { drop }
